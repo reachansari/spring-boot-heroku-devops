d641717e1acd053cb296bab405a5ff01b1d632cd
Thameem <reachansari@gmail.com>
2021-10-12 21:03:13 +0800

    first commit

---
+++ src/main/java/com/example/demo/heroku/SpringBootHerokuDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/heroku/SpringBootHerokuDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
