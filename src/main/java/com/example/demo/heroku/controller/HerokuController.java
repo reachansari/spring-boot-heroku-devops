080970cd1d35d919ecc8a048e882f5de6a709741
Thameem Ansari <reachansari@gmail.com>
2021-10-21 21:21:37 +0800

    Update HerokuController.java

--- src/main/java/com/example/demo/heroku/controller/HerokuController.java
+++ src/main/java/com/example/demo/heroku/controller/HerokuController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HerokuController {
 	@GetMapping
     public String hello() {
-        return "Welcome to Heroku";
+        return "Welcome to Heroku1";
     }
 
 }
